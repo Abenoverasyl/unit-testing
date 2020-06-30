9570bc0b94f7e02babfd44337ae2961992ec9009
YAben <abenov.erasyl@gmail.com>
2020-06-30 19:03:32 +0600

    feature MOCKITO - verify method calls

--- src/test/java/com/unit/testing/unittesting/business/ListMockTest.java
+++ src/test/java/com/unit/testing/unittesting/business/ListMockTest.java
@@ -5,12 +5,11 @@ import org.junit.Test;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class ListMockTest {
 
-    List mock = mock(List.class);
+    List<String> mock = mock(List.class);
 
     @Test
     public void test() {
@@ -31,5 +30,27 @@ public class ListMockTest {
         when(mock.get(0)).thenReturn("in28Minutes");
         assertEquals("in28Minutes", mock.get(0));
         assertEquals("in28Minutes", mock.get(1));
+    }
+
+    @Test
+    public void returnWithGenericParameters() {
+        when(mock.get(anyInt())).thenReturn("in28Minutes");
+        assertEquals("in28Minutes", mock.get(0));
+        assertEquals("in28Minutes", mock.get(1));
+    }
+
+    @Test
+    public void verificationBasics() {
+        // SUT
+        String value1 = mock.get(0);
+        String value2 = mock.get(1);
+
+        // verify
+        verify(mock).get(0);
+        verify(mock, times(2)).get(anyInt());
+        verify(mock, atLeast(1)).get(anyInt());
+        verify(mock, atLeastOnce()).get(anyInt());
+        verify(mock, atMost(2)).get(anyInt());
+        verify(mock, never()).get(2);
     }
 }
